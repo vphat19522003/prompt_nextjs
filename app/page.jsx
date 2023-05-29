7d3319cd07ff707f5d248cf6cf1675502985a8f5
vphat19522003 <19522003@gm.uit.edu.vn>
2023-05-29 13:37:21 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import Feed from "@components/Feed";
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient text-center">AI-Powred Prompts</span>
+        <p className="desc text-center">
+          Promptopia is an open-source AI prompting tool for modern world to
+          discover, create and share creative prompts
+        </p>
+        <Feed />
+      </h1>
+    </section>
+  );
+};
+
+export default Home;
